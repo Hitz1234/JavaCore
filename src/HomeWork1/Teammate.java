39d4231fd3cabd3b348393b886f895be94e133b8
hitz1234 <kaigorodovoleg93@gmail.com>
2022-10-06 04:48:53 +0700

    HomeWork1

---
+++ src/HomeWork1/Teammate.java
@@ -0,0 +1,16 @@
+package HomeWork1;
+
+public class Teammate {
+    private String name;
+    private int distance;
+
+    public Teammate(String name, int distance){
+    this.name = name;
+    this.distance = distance;
+    }
+
+    public String getName() {return name;}
+
+    public int getDistance() {return distance;}
+
+}
